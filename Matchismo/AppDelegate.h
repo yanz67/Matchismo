430a2aefec4714113bffc1ad0343c7e02c5cefb0
Yan Zverev <yanz67@gmail.com>
2014-12-14 10:47:04 -0500

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Yan Zverev on 12/14/14.
+//  Copyright (c) 2014 Yan Zverev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
